acc02ddaa6d94c7e7247f1228575c47f9bc8ec2e
NekuaTicoz <jeanguideau@hotmail.fr>
2023-09-18 09:17:39 +0200

    changement boutton Travail Repos

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ let chrono = document.getElementById("chrono");
 let temps = 5;
 let tempsTravail = 5;
 let tempsRepos = 4;
+const bouttonTravail = document.getElementById("travail");
+const bouttonRepos = document.getElementById("repos");
 compteurTravail = true; //si true alors travail sinon repos
 
-function affichageTemps() {
+function affichageTemps() {//affiche le temps en minutes:secondes
     let minutes = parseInt(temps / 60, 10);
     let secondes = parseInt(temps % 60, 10);
 
@@ -27,31 +29,41 @@ function travailRepos() { //verifie si on change de travail a repos et change de
 
     if (compteurTravail) {
         document.getElementById("travailRepos").textContent = "Travail";
+        changementBouttons();
         passageTempsTravailRepos();
     } else {
         document.getElementById("travailRepos").textContent = "Repos";
+        changementBouttons();
         passageTempsTravailRepos();
     }
 }
 
-function passageTempsTravailRepos() {
+function changementBouttons() {
+    bouttonTravail.disabled = !compteurTravail;
+    bouttonRepos.disabled = compteurTravail;
+}
+
+function passageTempsTravailRepos() { //gere le changement de temps
     if (compteurTravail) {
         temps = tempsTravail;
-        affichageTemps();
+        affichageTemps(); //affiche le temps de début d'une itération
     } else {
         temps = tempsRepos;
-        affichageTemps();
+        affichageTemps();//affiche le temps de début d'une itération (évite l'affichage d'un 00:00 et d'un secondes perdu pour l'itération suivante)
     }
 }
 
+//debut script
 
+
+changementBouttons();
 setInterval(
     () => {
-        affichageTemps();
+        affichageTemps();//affiche le temps
         if (temps == 0) {
-            travailRepos();
+            travailRepos(); //verifie si le temps de travail/repos est fini puis fait les changements
         }
     }, 1000);
-setInterval(diminuerTemps, 1000);
+setInterval(diminuerTemps, 1000);//fait diminuer le temps toute les secondes
 
 
