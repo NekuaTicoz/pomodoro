995257a3494df3c6c4d6d113adb08e44242e3373
NekuaTicoz <jeanguideau@hotmail.fr>
2023-09-18 08:30:58 +0200

    ajout chrono fonctionnel, commencement de la gestion travail repos

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
-chrono = document.getElementById("chrono");
-temps = 25;
+let chrono = document.getElementById("chrono");
+let temps = 5;
+compteurTravail = true ; //si true alors travail sinon repos
 
-function a() {
-    if (temps != 0) {
-        let minutes = Math.floor(temps / 60);
-        let secondes = temps % 60;
+
+
+function diminuerTemps() { //permet la gestion du chrono, diminue le temps et l'arrete a 00:00
+    let minutes = parseInt(temps / 60, 10)
+    let secondes = parseInt(temps % 60, 10)
+
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+
+    chrono.innerText = `${minutes}:${secondes}`
+    temps = temps <= 0 ? 0 : temps - 1
+}
+
+function travailRepos(){ //verifie si on change de travail a repos et change de travail a repos (vice et versa)
+    if (temps == 0){
+        if(compteurTravail){
+            compteurTravail=false;
+        }else{
+            compteurTravail=true;
+        }
+    }
         
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        secondes = secondes < 10 ? "0" + secondes : secondes;
-
-
-        chrono.textContent = minutes + ":" + secondes;
-        temps--;
-    } else {
-        chrono.innerHTML = "<i class='fa-brands fa-discord fa-bounce fa-2xl'></i>";
+    if (compteurTravail){
+        document.getElementById("travailRepos").textContent = travail;
+    }else{
+        document.getElementById("travailRepos").textContent = repos;
     }
 }
 
-setInterval(a, 1000);
+setInterval(diminuerTemps, 1000);
+travailRepos();
